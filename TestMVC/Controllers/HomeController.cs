753cd74329f18e2b72e51140bc6841f41fbf73ce
Alois De Wet <alois@netgen.co.za>
2021-06-26 00:01:34 +0200

    Add project files.

---
+++ TestMVC/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestMVC.Models;
+
+namespace TestMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<ProductModel> list = new List<ProductModel>();
+            for (int i = 0; i < 3; i++)
+            {
+                ProductModel product = new ProductModel()
+                {
+                    ID = Guid.NewGuid(),
+                    Name = "Name" + i.ToString(),
+                    Description = "Description" + i.ToString(),
+                    Price = "Price" + i.ToString()
+                };
+                list.Add(product);
+            }
+            return View(list);
+        }
+        [HttpPost]
+        public ActionResult AddToCart(FormCollection form)
+        {
+            var ids = form["item.ID"];
+            var strids = ids.Split(',');
+            Guid guid = Guid.Parse(strids[0]);
+            return View("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TestMVC/Models/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestMVC.Models
+{
+    public class ProductModel
+    {
+        public Guid ID{ get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+    }
+}
